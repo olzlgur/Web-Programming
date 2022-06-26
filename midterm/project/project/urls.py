8e331c145118e04f5459741e424b65b89d24ce4f
olzlg1 <olzlgur@naver.com>
2022-06-26 21:04:28 +0900

    2022-06-24

---
+++ midterm/project/project/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from firstapp import views
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
+    path('detail<str:id>', views.detail, name="detail"),
+    path('new/', views.new, name="new"),
+    path('create/', views.create, name="create"),
+    path('edit/<str:id>', views.edit, name="edit"),
+    path('update/<str:id>', views.update, name="update"),
+    path('delete/<str:id>', views.delete, name="delete"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
